8a2c47ac1cde84abc6bb7cb616df649c9f59a6bd
Jonathan Murray <murray.jonathaan@gmail.com>
2026-07-15 01:03:56 -0300

    config CORS

--- src/main/java/com/murray/dogs/dogAPI/controller/DogController.java
+++ src/main/java/com/murray/dogs/dogAPI/controller/DogController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://dog-finder-frontend.vercel.app"})
 @RestController
 @RequestMapping("/api/dogs")
 public class DogController {
